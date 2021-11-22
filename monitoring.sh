ae76e6bff22c9cd3027c7de2a48e79b04dc453b5
Stanley SERBIN <sserbin@e3r10p17.clusters.42paris.fr>
2021-11-22 23:21:53 +0100

    start givint it a style

--- monitoring.sh
+++ monitoring.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-hostnamectl | grep Architecture
-hostnamectl | grep Kernel
+hostnamectl | grep Architecture | tr -d " "
+hostnamectl | grep Kernel | tr -d " "
 cat /proc/cpuinfo | grep processor
-free -m
+echo -n "RAM: " && free -m | grep Mem | awk '{print ($3/$2)*100"%"}'
 df -h | grep root
 mpstat
 who -b
-sudo service lvm2 status
+#sudo service lvm2 status
 users | wc -c
 ps -eaho user
 ip a | grep inet | grep lo | grep host
